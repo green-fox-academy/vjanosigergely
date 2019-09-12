9e8901097a951af65955eefc16db3d34e1b93264
Gergely Janosi <v.janosi.gergely@gmail.com>
2019-09-12 20:39:09 +0200

    Pyramid solved

--- week-01/day-4/src/DrawPyramid.java
+++ week-01/day-4/src/DrawPyramid.java
@@ -6,13 +6,13 @@ public class DrawPyramid {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
 
-        for (int i = 0; i < n-1 ; i++) {
+        for (int i = 1; i <= n ; i++) {
 
-            for (int j = 1; j < i ; j++) {
+            for (int j = 1; j <= n-i ; j++) {
                 System.out.print(" ");
             }
 
-            for (int k = 1; k < i; k++) {
+            for (int k = 1; k <= ( 2*i -1); k++) {
                 System.out.print("*");
 
             }
