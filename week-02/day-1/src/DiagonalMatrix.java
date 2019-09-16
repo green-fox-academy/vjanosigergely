38cb609a19809e54056047c7652f65e98a03399b
Gergely Janosi <v.janosi.gergely@gmail.com>
2019-09-16 14:01:36 +0200

    colours

--- week-02/day-1/src/DiagonalMatrix.java
+++ week-02/day-1/src/DiagonalMatrix.java
@@ -3,8 +3,19 @@ public class DiagonalMatrix {
         int [][] matrix = new int[4][4];
 
         for (int i = 0; i < matrix.length ; i++) {
-            
+            for (int j = 0; j < matrix.length; j++) {
+                if (i == j) {
+                    matrix[i][j] = 1;
+                }
+            }
         }
+        for (int i = 0; i <matrix.length ; i++) {
+            for (int j = 0; j <matrix.length ; j++) {
+                System.out.print(matrix[i][j]);
+            }
+            System.out.println();
+        }
+
     }
 }
 // - Create (dynamically) a two dimensional array
---
+++ week-02/day-1/src/SwapElements.java
@@ -0,0 +1,9 @@
+public class SwapElements {
+    public static void main(String[] args) {
+        String[] abc = {"first", "second", "third"};
+
+    }
+}
+// Create an array variable named `abc`
+//   with the following content: `["first", "second", "third"]`
+// - Swap the first and the third element of `abc`
