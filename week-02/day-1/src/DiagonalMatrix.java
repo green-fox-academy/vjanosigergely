f7fbdeb8b8297d8e857497d174ff3b60b9fb1b85
Gergely Janosi <v.janosi.gergely@gmail.com>
2019-09-16 12:42:53 +0200

    One dimensional Array exercises completed

---
+++ week-02/day-1/src/DiagonalMatrix.java
@@ -0,0 +1,18 @@
+public class DiagonalMatrix {
+    public static void main(String[] args) {
+        int [][] matrix = new int[4][4];
+
+        for (int i = 0; i < matrix.length ; i++) {
+            
+        }
+    }
+}
+// - Create (dynamically) a two dimensional array
+//   with the following matrix. Use a loop!
+//
+//   1 0 0 0
+//   0 1 0 0
+//   0 0 1 0
+//   0 0 0 1
+//
+// - Print this two dimensional array to the output
