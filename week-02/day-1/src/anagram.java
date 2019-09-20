5dec30c3f1145dee78018917710bc1b7791ca6ba
Gergely Janosi <v.janosi.gergely@gmail.com>
2019-09-20 14:23:50 +0200

    anagramfinder

--- week-02/day-1/src/anagram.java
+++ week-02/day-1/src/anagram.java
@@ -1,3 +1,5 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class anagram {
@@ -7,14 +9,42 @@ public class anagram {
         String word1 = scanner.nextLine();
         String word2 = scanner.nextLine();
 
-        boolean isAnagram;
 
         char[] firstWord = word1.toCharArray();
         char[] secondWord = word2.toCharArray();
 
+        System.out.println(anagramFinder(firstWord,secondWord));
 
 
     }
 
-    public static boolean anagramFinder
+    public static boolean anagramFinder(char[] wordArray1, char[] wordArray2) {
+        Arrays.sort(wordArray1);
+        Arrays.sort(wordArray2);
+        boolean isAnagram;
+
+        if (wordArray1.length != wordArray2.length) {
+            isAnagram = false;
+        } else {
+            int truthCounter = 0;
+            for (int i = 0; i < wordArray1.length; i++) {
+                if (wordArray1[i] == wordArray2[i]) {
+                    truthCounter = truthCounter + 1;
+                }
+
+            }
+            if (truthCounter == wordArray1.length) {
+                isAnagram = true;
+
+            }else{
+                isAnagram = false;
+            }
+        }
+
+        return isAnagram;
+
+        }
+
+
+
 }
