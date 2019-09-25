98e41efefdd6de622700ab9305ee81f5b0bd35ac
Gergely Janosi <v.janosi.gergely@gmail.com>
2019-09-25 12:57:45 +0200

    Sharpie solved

---
+++ week-03/day-2/src/sharpie/Main.java
@@ -0,0 +1,12 @@
+package sharpie;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Sharpie red = new Sharpie("red", 1.02f);
+
+        red.use();
+        red.use();
+      
+    }
+}
