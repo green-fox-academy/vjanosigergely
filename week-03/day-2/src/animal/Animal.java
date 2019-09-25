b0d5f012031deaea73ff77602ce3e7fcffa66a69
Gergely Janosi <v.janosi.gergely@gmail.com>
2019-09-25 12:48:16 +0200

    Animal solved

--- week-03/day-2/src/animal/Animal.java
+++ week-03/day-2/src/animal/Animal.java
@@ -1,4 +1,29 @@
 package animal;
 
 public class Animal {
+
+    int hunger = 50;
+    int thirst = 50;
+
+    public void eat (){
+        hunger = hunger - 1;
+    }
+
+    public void drink(){
+        thirst = thirst - 1;
+    }
+
+    public void play (){
+        hunger +=1;
+        thirst +=1;
+    }
+
 }
+
+
+//Every animal has a hunger value, which is a whole number
+//Every animal has a thirst value, which is a whole number
+//when creating a new animal object these values are created with the default 50 value
+//Every animal can eat() which decreases their hunger by one
+//Every animal can drink() which decreases their thirst by one
+//Every animal can play() which increases both by one
--- week-03/day-2/src/animal/Main.java
+++ week-03/day-2/src/animal/Main.java
@@ -1,4 +1,15 @@
 package animal;
 
 public class Main {
+    public static void main(String[] args) {
+        Animal zebra = new Animal();
+        Animal monkey = new Animal();
+
+        zebra.eat();
+        monkey.drink();
+        monkey.drink();
+        monkey.play();
+        zebra.play();
+
+    }
 }
