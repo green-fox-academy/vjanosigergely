b88a310c32e40c203c156fe0c76aa1d29f9fba39
Gergely Janosi <v.janosi.gergely@gmail.com>
2019-10-21 16:33:57 +0200

    exercise 9 solved, case snsitivity and white spaces removed

---
+++ week-06/day-1/src/Exercise9.java
@@ -0,0 +1,22 @@
+import java.util.HashMap;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class Exercise9 {
+
+  public static void main(String[] args) {
+    String text = "My Hovercraft is full of eels";
+
+    HashMap<Character, Integer> frequency = (HashMap<Character, Integer>) text.toLowerCase().replaceAll(" ", "").chars()
+        .mapToObj(c -> (char) c)
+        .collect(Collectors.groupingBy(Function.identity(),Collectors.summingInt(c ->1)));
+  }
+
+}
+
+
+//Write a Stream Expression to find the frequency of characters in a given string!
+//Map<Character, Integer> frequency =
+//            str.chars()
+//               .mapToObj(c -> (char)c)
+//               .collect(Collectors.groupingBy(Function.identity(), Collectors.summingInt(c -> 1)));
