4b7946bbe2c3701ffb2b7ec2a18ce94cf8d6fa04
Gergely Janosi <v.janosi.gergely@gmail.com>
2019-10-21 14:39:33 +0200

    5th solved

--- week-06/day-1/src/Exercise4.java
+++ week-06/day-1/src/Exercise4.java
@@ -6,7 +6,6 @@ public class Exercise4 {
 
   public static void main(String[] args) {
     List<Integer> numbers = Arrays.asList(1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14);
-    int length = numbers.size();
 
     OptionalDouble oddAverage = numbers.stream()
         .filter(n -> n % 2 != 0)
--- week-06/day-1/src/Exercise5.java
+++ week-06/day-1/src/Exercise5.java
@@ -1,7 +1,22 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.OptionalDouble;
+import java.util.OptionalInt;
+
 public class Exercise5 {
 
   public static void main(String[] args) {
-    
+    List<Integer> numbers = Arrays.asList(5, 9, 1, 2, 3, 7, 5, 6, 7, 3, 7, 6, 8, 5, 4, 9, 6, 2);
+
+    int oddSum = numbers.stream()
+        .filter(n -> n % 2 != 0)
+        .mapToInt(i -> i)
+        .sum();
+
   }
 
 }
+
+//Write a Stream Expression to get the sum of the odd numbers in the following list:
+//
+//List<Integer> numbers = Arrays.asList(5, 9, 1, 2, 3, 7, 5, 6, 7, 3, 7, 6, 8, 5, 4, 9, 6, 2);
