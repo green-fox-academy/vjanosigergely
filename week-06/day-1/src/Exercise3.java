c1d791d32b27f7ba24b0155eb85c04c6b0ddaab3
Gergely Janosi <v.janosi.gergely@gmail.com>
2019-10-21 13:06:23 +0200

    2nd exercise

---
+++ week-06/day-1/src/Exercise3.java
@@ -0,0 +1,8 @@
+public class Exercise3 {
+
+}
+
+
+//Write a Stream Expression to find which number squared value is more then 20 from the following list:
+//
+//List<Integer> numbers = Arrays.asList(3, 9, 2, 8, 6, 5);
