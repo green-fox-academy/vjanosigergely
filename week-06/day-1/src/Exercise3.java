7e87034460b925d1463b79229e0055e8217d3032
Gergely Janosi <v.janosi.gergely@gmail.com>
2019-10-21 14:16:13 +0200

    3rd exercise solved

--- week-06/day-1/src/Exercise3.java
+++ week-06/day-1/src/Exercise3.java
@@ -1,4 +1,18 @@
+import static java.lang.Math.pow;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Exercise3 {
+
+  public static void main(String[] args) {
+    List<Integer> numbers = Arrays.asList(3, 9, 2, 8, 6, 5);
+
+    List<Integer> squaredNumbers = numbers.stream()
+        .filter(n -> Math.pow(n,2) > 20)
+        .collect(Collectors.toList());
+  }
 
 }
 
