d208f9227f526d43a5468d6eeb0c8163bd4db2f5
Gergely Janosi <v.janosi.gergely@gmail.com>
2019-10-21 14:33:44 +0200

    4th exercise solved with OptionalDouble

--- week-06/day-1/src/Exercise4.java
+++ week-06/day-1/src/Exercise4.java
@@ -1,3 +1,23 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.OptionalDouble;
+
 public class Exercise4 {
 
+  public static void main(String[] args) {
+    List<Integer> numbers = Arrays.asList(1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14);
+    int length = numbers.size();
+
+    OptionalDouble oddAverage = numbers.stream()
+        .filter(n -> n % 2 != 0)
+        .mapToDouble(i -> i)
+        .average();
+
+  }
+
 }
+
+
+//Write a Stream Expression to get the average value of the odd numbers from the following list:
+//
+//List<Integer> numbers = Arrays.asList(1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14);
---
+++ week-06/day-1/src/Exercise5.java
@@ -0,0 +1,7 @@
+public class Exercise5 {
+
+  public static void main(String[] args) {
+    
+  }
+
+}
