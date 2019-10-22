6371be928d9251c133a841944ba2e73fac6bb349
Gergely Janosi <v.janosi.gergely@gmail.com>
2019-10-22 15:55:32 +0200

    HelloMultiLanguageController - color can be set by user

--- week-06/day-2/src/main/java/com/greenfoxacademy/springstart/controllers/HelloWebController.java
+++ week-06/day-2/src/main/java/com/greenfoxacademy/springstart/controllers/HelloWebController.java
@@ -3,21 +3,23 @@ package com.greenfoxacademy.springstart.controllers;
 import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.stereotype.Controller;
 //import org.springframework.ui.Model;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HelloWebController {
 
   AtomicLong counter = new AtomicLong();
 
   @RequestMapping(value ="/web/greeting")
-  public String greeting(@RequestParam String name) {
-    //model.addAttribute("name", " World");
+  public String greeting(@RequestParam String name, Model model) {
+    model.addAttribute("name", name);
+    model.addAttribute("counter", counter.getAndIncrement());
     //Hello, Your name! This site was loaded 3 times since last server start.
-    String hello = "Hello " + name + "! This site was loaded " + counter.addAndGet(1)+ " times since last server start";
-    return hello;
+
+    return "greeting";
   }
 
 }
