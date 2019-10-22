4f8a516dca2ec146e636b9e171502568d7661077
Gergely Janosi <v.janosi.gergely@gmail.com>
2019-10-22 11:57:26 +0200

    Hello User REST App

--- week-06/day-2/src/main/java/com/greenfoxacademy/springstart/controllers/HelloRESTController.java
+++ week-06/day-2/src/main/java/com/greenfoxacademy/springstart/controllers/HelloRESTController.java
@@ -1,13 +1,14 @@
 package com.greenfoxacademy.springstart.controllers;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloRESTController {
   @RequestMapping(value="/greeting")
-public Greeting greeting(){
-  return new Greeting(28,"Wabbalabadubdub");
+public Greeting greeting(@RequestParam String name){
+  return new Greeting(28,"Hello " + name);
 }
 }
 
