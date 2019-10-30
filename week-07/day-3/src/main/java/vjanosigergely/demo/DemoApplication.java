4f8453261bdc82c7eeb5a1a79ab7ece2c36da057
Gergely Janosi <v.janosi.gergely@gmail.com>
2019-10-30 12:57:25 +0100

    Hello DI project

---
+++ week-07/day-3/src/main/java/vjanosigergely/demo/DemoApplication.java
@@ -0,0 +1,25 @@
+package vjanosigergely.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication implements CommandLineRunner {
+  private Printer printer;
+
+  @Autowired
+  DemoApplication(Printer printer){
+    this.printer = printer;
+  }
+
+  public static void main(String[] args) {
+    SpringApplication.run(DemoApplication.class, args);
+  }
+
+  @Override
+  public void run(String... args) throws Exception {
+  printer.log("hello");
+  }
+}
