a027bdce205eecc4ab016d42710a4bf5a326d664
Gergely Janosi <v.janosi.gergely@gmail.com>
2019-11-05 17:44:21 +0100

    delete and add function

--- week-08/day-2/connectionWithMySQL/src/main/java/com/vjanosigergely/connectsql/controllers/TodoController.java
+++ week-08/day-2/connectionWithMySQL/src/main/java/com/vjanosigergely/connectsql/controllers/TodoController.java
@@ -1,10 +1,14 @@
 package com.vjanosigergely.connectsql.controllers;
 
+import com.vjanosigergely.connectsql.models.Todo;
 import com.vjanosigergely.connectsql.repository.TodoRepoInterface;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -33,6 +37,24 @@ public class TodoController {
     }
 
   }
+
+  @GetMapping(value = "/add")
+  public String showAddForm(@ModelAttribute(name = "newtodo") Todo todo){
+    return "add";
+  }
+
+  @PostMapping(value = "/add")
+  public String addNew(@ModelAttribute Todo todo){
+    myRepo.save(todo);
+    return "redirect:/todo/list";
+  }
+
+  @GetMapping(value = "/delete/{id}")
+  public String delete(@PathVariable(name = "id") Long id){
+    myRepo.deleteById(id);
+    return "redirect:/todo/list";
+  }
+
 
 
 }
